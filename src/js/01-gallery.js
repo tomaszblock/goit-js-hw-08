64d6f188c31ae086f4a07baca6b044daa2185b54
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2024-02-04 16:03:27 +0100

    part 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const result = [];
+
+for (let item of galleryItems) {
+  let newItem = document.createElement('div');
+  newItem.classList.add('gallery__item');
+
+  let newLink = document.createElement('a');
+  newLink.classList.add('gallery__link');
+  newLink.setAttribute('href', item.original);
+
+  let newImage = document.createElement('img');
+  newImage.classList.add('gallery__image');
+  newImage.setAttribute('src', item.preview);
+  newImage.setAttribute('alt', item.description);
+  newImage.setAttribute('data-source', item.original);
+
+  newLink.append(newImage);
+  newItem.append(newLink);
+  result.push(newItem);
+}
+
+gallery.append(...result);
+
+let = lightbox = new SimpleLightbox('.gallery a');
