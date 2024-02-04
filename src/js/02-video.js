b6a62f5fb3f970e0ec3533c23dc5be8ff9e376c3
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2024-02-04 17:36:03 +0100

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
 
+const vid = document.querySelector('#vimeo-player');
+
+const player = new Player(vid, {});
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+let localStorageKey = 'videoplayer-current-time';
+localStorage.getItem(
+  localStorageKey,
+  player.on('timeupdate', function (e) {})
+);
