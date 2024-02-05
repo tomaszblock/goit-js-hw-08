6e489b484f759ba7bfeba4a1f5360740193e69cf
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2024-02-05 14:00:16 +0100

    fixed request 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,10 +8,10 @@ const player = new Player(vid, {});
 // player.on('play', function () {
 //   console.log('played the video!');
 // });
-
-player.on('timeupdate', function (data) {
+const test = function (data) {
   console.log(data.seconds);
   localStorage.setItem('videoplayer-current-time', data.seconds);
-});
+};
+player.on('timeupdate', throttle(test, 10000));
 
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
