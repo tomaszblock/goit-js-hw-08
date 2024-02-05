039f7ea6d844d68b73be3f0788a6ebb91fd81d50
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2024-02-05 13:50:35 +0100

    request 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
 import throttle from 'lodash.throttle';
+
+const emailWIndow = document.querySelector('.feedback-form input');
+const messageWindow = document.querySelector('.feedback-form textarea');
+const form = document.querySelector('.feedback-form');
+
+const result = {};
+
+const emailFunc = () => {
+  result.email = emailWIndow.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(result));
+  //   console.log(result);
+};
+const messageFunc = () => {
+  result.message = messageWindow.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(result));
+  //   console.log(result);
+};
+
+emailWIndow.addEventListener('keyup', throttle(emailFunc, 500));
+messageWindow.addEventListener('keyup', throttle(messageFunc, 500));
+JSON.parse(localStorage.getItem('feedback-form-state'));
+
+const stored = localStorage.getItem('feedback-form-state');
+const parsed = JSON.parse(stored);
+
+if (parsed.email !== null) {
+  emailWIndow.value = parsed.email;
+} else {
+  return;
+}
+if (parsed.message !== null) {
+  messageWindow.value = parsed.message;
+} else {
+  return;
+}
+
+function send(event) {
+  event.preventDefault();
+  console.log(parsed);
+  form.reset();
+}
+
+form.addEventListener('submit', send);
