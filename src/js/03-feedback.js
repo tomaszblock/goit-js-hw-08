5a9e4f25b9e34d55bcf6aae2cb2f30c2cc44e0db
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2024-02-09 16:18:30 +0100

    fixed ad3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,66 +1,32 @@
 import throttle from 'lodash.throttle';
 
+const emailWindow = document.querySelector('.feedback-form input');
+const messageWindow = document.querySelector('.feedback-form textarea');
+const form = document.querySelector('.feedback-form');
 
- const emailWIndow = document.querySelector('.feedback-form input');
- const messageWindow = document.querySelector('.feedback-form textarea');
- const form = document.querySelector('.feedback-form');
-const result = {}
- form.addEventListener('input', (e) => {
-if (e.target === emailWIndow) {
- result.email = e.target.value
-  console.log(result)
-} else if (e.target === messageWindow) {
-  result.message = e.target.value
-console.log(result)
+form.addEventListener('input', () => {
+  const result = {
+    email: emailWindow.value,
+    message: messageWindow.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(result));
+});
+
+let stored = localStorage.getItem('feedback-form-state');
+let parsed = stored ? JSON.parse(stored) : {};
+
+emailWindow.value = parsed.email || '';
+messageWindow.value = parsed.message || '';
+
+function send(event) {
+  if (emailWindow.value === '' || messageWindow.value === '') {
+    emailWindow.setAttribute('placeholder', 'Fill it!');
+    messageWindow.setAttribute('placeholder', 'Fill it!');
+  } else {
+    event.preventDefault();
+    console.log(parsed);
+    form.reset();
+  }
 }
- })
 
-
-
-
-
-
-// option 2
-
-// const emailWIndow = document.querySelector('.feedback-form input');
-// const messageWindow = document.querySelector('.feedback-form textarea');
-// const form = document.querySelector('.feedback-form');
-
-// const result = {};
-
-// const emailFunc = () => {
-//   result.email = emailWIndow.value;
-//   localStorage.setItem('feedback-form-state', JSON.stringify(result));
-//     console.log(result);
-// };
-// const messageFunc = () => {
-//   result.message = messageWindow.value;
-//   localStorage.setItem('feedback-form-state', JSON.stringify(result));
-//     console.log(result);
-// };
-
-// emailWIndow.addEventListener('keyup', throttle(emailFunc, 500));
-// messageWindow.addEventListener('keyup', throttle(messageFunc, 500));
-// JSON.parse(localStorage.getItem('feedback-form-state'));
-
-// const stored = localStorage.getItem('feedback-form-state');
-// const parsed = JSON.parse(stored);
-
-// if (parsed.email !== null) {
-//   emailWIndow.value = parsed.email;
-// } else {
-//   return;
-// }
-// if (parsed.message !== null) {
-//   messageWindow.value = parsed.message;
-// } else {
-//   return;
-// }
-
-// function send(event) {
-//   event.preventDefault();
-//   console.log(parsed);
-//   form.reset();
-// }
-
-// form.addEventListener('submit', send);
+form.addEventListener('submit', send);
