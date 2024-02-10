0385cb3d20dd21db8cb021a79c26bc5fa0b11cac
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2024-02-10 13:09:50 +0100

    ad 3 fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,15 +3,14 @@ import throttle from 'lodash.throttle';
 const emailWindow = document.querySelector('.feedback-form input');
 const messageWindow = document.querySelector('.feedback-form textarea');
 const form = document.querySelector('.feedback-form');
-
-form.addEventListener('input', () => {
+const listener = () => {
   const result = {
     email: emailWindow.value,
     message: messageWindow.value,
   };
   localStorage.setItem('feedback-form-state', JSON.stringify(result));
-});
-
+};
+form.addEventListener('input', throttle(listener, 500));
 let stored = localStorage.getItem('feedback-form-state');
 let parsed = stored ? JSON.parse(stored) : {};
 
@@ -19,13 +18,21 @@ emailWindow.value = parsed.email || '';
 messageWindow.value = parsed.message || '';
 
 function send(event) {
-  if (emailWindow.value === '' || messageWindow.value === '') {
+  if (emailWindow.value.trim() === '' || messageWindow.value.trim() === '') {
+    event.preventDefault();
     emailWindow.setAttribute('placeholder', 'Fill it!');
     messageWindow.setAttribute('placeholder', 'Fill it!');
   } else {
+    const object = {
+      email: emailWindow.value,
+      message: messageWindow.value,
+    };
+    console.log(object);
     event.preventDefault();
-    console.log(parsed);
     form.reset();
+    localStorage.clear();
+    emailWindow.removeAttribute('placeholder');
+    messageWindow.removeAttribute('placeholder');
   }
 }
 
